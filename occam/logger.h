5a1cdf9b729a8fbca0a8ee4da205d45e006d79d6
Alexander Kass <kass.soft@gmail.com>
2013-09-01 16:57:02 +0400

    fix

--- occam/logger.h
+++ occam/logger.h
@@ -6,26 +6,52 @@ typedef enum occam_log_level_tag
    OCCAM_LOG_LEVEL_INFO,
    OCCAM_LOG_LEVEL_WARN,
    OCCAM_LOG_LEVEL_ERROR,
+
+   OCCAM_LOG_LEVEL_COUNT
 } occam_log_level_t;
 
 typedef
-   void (*occam_log_handler)(void * userdata,
+   void (*occam_log_handler_t)(void * userdata,
              const occam_log_level_t level,
-             const char * const area,
-             const char * const msg);
+             const char * area,
+             const char * fmt,
+             va_list args);
 
 typedef struct occam_logger_tag
 {
-   occam_log_handler handler;
+   occam_log_handler_t handler;
    void *userdata;
 } occam_logger_t;
 
+static const char * const _occam_log_level_name[OCCAM_LOG_LEVEL_COUNT] = {
+   "DEBUG",
+   "INFO",
+   "WARN",
+   "ERROR"
+};
+
 //TODO: add some cases
-#define OCCAM_CURRENT_FUNCTION __FUNC__
+#define OCCAM_CURRENT_FUNCTION __func__
 
-#define occam_log(LOG, LVL, MSG) (LOG)->handler((LOG)->userdata, LVL, OCCAM_CURRENT_FUNCTION, MSG)
-#define occam_log_d(LOG, MSG) occam_log(LOG, OCCAM_LOG_LEVEL_DEBUG, MSG)
-#define occam_log_i(LOG, MSG) occam_log(LOG, OCCAM_LOG_LEVEL_INFO,  MSG)
-#define occam_log_w(LOG, MSG) occam_log(LOG, OCCAM_LOG_LEVEL_WARN,  MSG)
-#define occam_log_e(LOG, MSG) occam_log(LOG, OCCAM_LOG_LEVEL_ERROR, MSG)
+void occam_logv(occam_logger_t * log, occam_log_level_t lvl,
+   const char * area, const char * fmt, va_list args)
+{
+   if(log)
+      log->handler(log->userdata, lvl, area, fmt, args);
+}
+void occam_log(occam_logger_t * log, occam_log_level_t lvl,
+   const char * area, const char * fmt, ...)
+{
+   va_list ap;
+   va_start(args, fmt);
+   occam_logv(log, lvl, area, fmt, args);
+   va_end(args);
+}
 
+#define occam_current_log(LOG, LVL, FMT, ...) occam_log(LOG, LVL, OCCAM_CURRENT_FUNCTION, FMT, __VA_ARGS__)
+#define occam_log_d(LOG, FMT, ...) occam_log(LOG, OCCAM_LOG_LEVEL_DEBUG, FMT, __VA_ARGS__)
+#define occam_log_i(LOG, FMT, ...) occam_log(LOG, OCCAM_LOG_LEVEL_INFO,  FMT, __VA_ARGS__)
+#define occam_log_w(LOG, FMT, ...) occam_log(LOG, OCCAM_LOG_LEVEL_WARN,  FMT, __VA_ARGS__)
+#define occam_log_e(LOG, FMT, ...) occam_log(LOG, OCCAM_LOG_LEVEL_ERROR, FMT, __VA_ARGS__)
+
+#define occam_log_level_name(LVL) (_occam_log_level_name[LVL])
